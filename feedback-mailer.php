a22f438dae58a8cc3a4f2cd26e0dfe2c9b0a8d2d
Mohammed <Mohammed23000@student.hamk.fi>
2024-02-09 08:00:02 +0200

    database

--- feedback-mailer.php
+++ feedback-mailer.php
@@ -1,21 +1,22 @@
-<!-- After 10 seconds, redirect to a different page. In this case, Google. -->
-<meta http-equiv='refresh' content='10; url=https://google.com/'>
-<meta charset="UTF-8" />
 <!-- The PHP logic begins. -->
 <?php
 // Time to get the variables from the user’s request. Once we execute these four commands, we’ll have the user’s data in our variables.
-$name = $_POST['name'];
-$email = $_POST['email'];
-$header = $_POST['header'];
-$message = $_POST['message'];
-// Creating the email message that we’ll send.
-$mes = "Name: $name \nE-mail: $email \nSubject: $header \nMessage: $message";
-// Trying to send the message using the PHP mailer module.
-$send = mail ($email,$header,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
-// If send successful:
-if ($send == 'true')
-// ‘Echo’ returns some text back to the webpage.
-{echo "Message sent";}
-// If send fails:
-else {echo "Something went wrong";}
-?>
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$header = $_POST['header'];
+	$message = $_POST['message'];
+	// connecting to db server
+	include 'database.php';
+	// insert data to db
+	$sql = "INSERT INTO Feedback(name,email,title,message)
+			VALUES ('$name','$email','$header','$message')";
+	// if query success
+	if ($conn->query($sql)==true) {
+		echo "Your data has been recorded successfully.";
+	} else {
+		// Error: 
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
--- index.php
+++ index.php
@@ -3,23 +3,28 @@
     <!-- Header -->
 <?php
  include_once('header.php');
+ include 'database.php';
 ?>
 <body>
 
-<form action="feedback-mailer.PHP" method="post" name="form">
-  <!—- Inside the form, lets put some input fields. -->
+<form action="" method="post" name="form">
+  <!-- Inside the form, lets put some input fields. -->
   <input name="name" type="text" placeholder="Your name"/>
   <br>
   <input name="email" type="text" placeholder="Email"/>
   <br>
   <input size="30" name="header" type="text" placeholder="Subject"/>
   <br>
-  <textarea cols="32" name="message" rows="5"> Your message
-  </textarea>
+  <textarea cols="32" name="message" rows="5" placeholder="Your message"></textarea>
   <br>
-  <!—- Lets add a button that submits this form to the server. -->
-  <input type="submit" value="Submit" />
+  <!-- Lets add a button that submits this form to the server. -->
+  <input type="submit" name="submit" value="Submit" />
 </form>
+<?php
+include 'feedback-mailer.php';
+?>
+
+
     <div class="row filler-row-100">
         <!-- row for space -->
     </div>
